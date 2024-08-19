ad968bfd9cf019d00685f7220beb4c3ebe60e37e
Arles Talavera <it-outtasking10@grupopurdy.com>
2024-08-18 20:56:11 -0600

    feat: implement role-based authorization and update security configuration to use PasswordEncoder

--- src/main/java/com/technicaltest/shoppingcart/config/SecurityConfig.java
+++ src/main/java/com/technicaltest/shoppingcart/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,13 +22,35 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())  // Disable CSRF protection
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/products").authenticated()  // Requires authentication
-                                .requestMatchers("/api/orders/**").authenticated()  // Requires authentication
-                                .requestMatchers("/api/payments/**").authenticated() // Requires authentication
+                                .requestMatchers("/api/products").hasAnyRole("USER", "ADMIN")  // Only users with the USER rol can access
+                                .requestMatchers("/api/orders/**").hasRole("ADMIN")  // Only users with the ADMIN rol can access
+                                .requestMatchers("/api/payments/**").hasAnyRole("USER", "ADMIN") // Both roles can access
                                 .anyRequest().authenticated()  // Requires authentication for other request
                 )
                 .httpBasic(customizer -> {});
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("shoppingUser")
+                .password(passwordEncoder.encode("Sh0ppingUs3rS3cur3!"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("shoppingAdmin")
+                .password(passwordEncoder.encode("Sh0ppingC@rtS3cur3!"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
